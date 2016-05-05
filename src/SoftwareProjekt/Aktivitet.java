bcdfba28d2c401a5dfaa2410c0534e93952f9ce5
s144122 <s144122@student.dtu.dk>
2016-05-05 16:55:18 +0200

    f

--- src/SoftwareProjekt/Aktivitet.java
+++ src/SoftwareProjekt/Aktivitet.java
@@ -8,18 +8,16 @@ public class Aktivitet {
 	public double Tid;
 	public static List<TimeObjekt> to =  new ArrayList<>();
 	public String index;
-	
+
 	public static Oprettelse opret = new Oprettelse();
 	public static List<Medarbejder> list = opret.getmedarbejderList();
 
 	public static void main(String args[]){
 		Medarbejder k = opret.opretMedarbejder("Kristian Krarup", "KK", "23346859", "s144122@student.dtu.dk", "120894-xxxx", true);
 		Medarbejder d = opret.opretMedarbejder("Dejar Tarik", "DT", "42723071", "s154414@student.dtu.dk", "301196-xxxx", false);
-		
-		TimeObjekt kk = new TimeObjekt(k,5);
-		TimeObjekt dd = new TimeObjekt(d,5);
-		TilfoejMedarbejder(kk);
-		TilfoejMedarbejder(dd);
+
+		TilfoejMedarbejder(k);
+		TilfoejMedarbejder(d);
 		PrintTimeObjekt(to);
 	}
 
@@ -41,14 +39,19 @@ public class Aktivitet {
 		}
 	}
 
-	public static void TilfoejMedarbejder(TimeObjekt Medarbejder){
-		to.add(Medarbejder); 
-//		if(list.contains(Medarbejder)){
-//			to.add(Medarbejder); 
-//		}
-//		else{
-//			System.out.println("FEJL: Medarbejderen du prøver at tilføje til aktiviteten er ikke i listen over medarbejder.");
-//		}
+	public static void TilfoejMedarbejder(Medarbejder medarbejder){
+		boolean x = false;
+		for(int i = 0; i < list.size(); i++){
+			if (list.get(i) == medarbejder){
+				TimeObjekt TO = new TimeObjekt(medarbejder,0);
+				to.add(TO);
+				x = true;
+			}
+		}
+		if (x = false){
+			System.out.println("FEJL: Medarbejderen du prøver at tilføje til aktiviteten er ikke i listen over medarbejder.");
+		}
+
 	}
 
 	public void FjernMedarbejder(TimeObjekt Medarbejder){
@@ -60,9 +63,15 @@ public class Aktivitet {
 
 	}
 	public static void PrintTimeObjekt(List<TimeObjekt> liste){
-		for(int i = 0; i < to.size(); i++) {
-			System.out.println(to.get(i).m.getNavn() + " | " + to.get(i).m.Intialer + " | "  + to.get(i).m.getTlf() + " | "  + to.get(i).m.getEmail() + " | "  + to.get(i).m.getCPR() + " | "  + to.get(i).m.getProjektleder() + " | " + to.get(i).getTid() );
+		if (to.size() == 0){
+			System.out.println("Der er ikke oprettet nogle medarbejdere");
+		}
+		else{
+			for(int i = 0; i < to.size(); i++) {
+
+				System.out.println(to.get(i).m.getNavn() + " | " + to.get(i).m.Intialer + " | "  + to.get(i).m.getTlf() + " | "  + to.get(i).m.getEmail() + " | "  + to.get(i).m.getCPR() + " | "  + to.get(i).m.getProjektleder() + " | " + to.get(i).getTid() );
+			}
 		}
 	}
-	
+
 }
