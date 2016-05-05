eeb25277436673df50ffc25a54f4e608ffa018d9
s144122 <s144122@student.dtu.dk>
2016-05-05 11:53:43 +0200

    Login

---
+++ src/SoftwareProjekt/Login.java
@@ -0,0 +1,34 @@
+package SoftwareProjekt;
+
+import java.util.List;
+
+public class Login {
+	private Oprettelse opret = new Oprettelse(); 
+
+	public boolean projektlederlogin(String intialer ){
+		opret = new Oprettelse();
+		List<Medarbejder> list = opret.getmedarbejderList();
+
+		if (list.contains(intialer) == true){
+			for(Medarbejder i : list ){
+				if (i.equals(intialer) && (i.Projektleder == true)){
+					return true;
+				}
+			}
+		}
+		return false;
+
+	}
+	public boolean medarbejderlogin(String intialer){
+		opret = new Oprettelse();
+		List<Medarbejder> list = opret.getmedarbejderList();
+		if (list.contains(intialer) == true){
+			for(Medarbejder i : list ){
+				if (i.equals(intialer) && (i.Projektleder == false)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
