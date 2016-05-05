a4c95eaa7e6067b10c7438e6fb530efd17e137ad
ultracake <jackievuong@live.dk>
2016-05-05 21:00:43 +0200

    gui

---
+++ src/SoftwareProjekt/hovedGUI.java
@@ -0,0 +1,36 @@
+package SoftwareProjekt;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+
+//Lavet af Jackie Vuong: studie nr. s153121 
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class hovedGUI extends JFrame implements ActionListener
+{
+	
+	
+	public hovedGUI()
+	{
+		
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		
+		
+	}
+	
+
+}
