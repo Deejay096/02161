c753e0ba0c68c6a540d7aa1a69d85a3c3ca84ace
ultracake <jackievuong@live.dk>
2016-05-05 16:26:08 +0200

    ny test

---
+++ Tests/TestSoftware/MedarbejderTest.java
@@ -0,0 +1,40 @@
+package TestSoftware;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import SoftwareProjekt.Medarbejder;
+
+public class MedarbejderTest {
+	
+	String Navn = "Larry Jensen";
+	String Initialer = "LJ";
+	String Tlf = "12345678";
+	String Email = "LL@softwarehuset.dk";
+	String CPR = "1401881111";
+	
+	@Test
+	public void medarbejderSygTest() {
+		
+		
+		Medarbejder syg = new Medarbejder(Navn, Initialer, Tlf, Email, CPR, false);
+		
+		int tid = 3; // Vi tester med 3 timer
+		
+		syg.tidSyg(syg, tid); //Det var første test 
+		
+	}
+	
+	@Test
+	public void medarbejderFerieTest(){
+		
+		Medarbejder ferie  = new Medarbejder(Navn, Initialer, Tlf, Email, CPR, false);
+		
+		int tid = 168; // Vi tester med 1 uge = 168 timer
+		
+		ferie.tidFerie(ferie, tid);
+		
+	}
+
+}
