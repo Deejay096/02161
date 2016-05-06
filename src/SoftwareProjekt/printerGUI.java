b461de33ea92801cd2b6cab86b4d9842f7f0c28d
ultracake <jackievuong@live.dk>
2016-05-06 13:31:13 +0200

    gui2

--- src/SoftwareProjekt/printerGUI.java
+++ src/SoftwareProjekt/printerGUI.java
@@ -11,10 +11,14 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+//Lavet af Jackie Vuong: studie nr. s153121 
 
 public class printerGUI extends JFrame implements ActionListener
 {
-	JButton printK; // vores knap
+	JButton printM, printA, printP ; // vores knapper
+	JTextField printT; // vores text field
 
 	public printerGUI()
 	{
@@ -22,22 +26,54 @@ public class printerGUI extends JFrame implements ActionListener
 	     Dimension spaceDimension = new Dimension(100, 100);
 	     getContentPane().setLayout(new BorderLayout());
 	     
-	     printK = new JButton("Printer Liste");
-	     printK.addActionListener(this);
-	     printK.setMaximumSize(BSize);
-	     printK.setAlignmentX(Component.CENTER_ALIGNMENT);
+	     printM = new JButton("Print Medarbejder Liste");
+	     printM.addActionListener(this);
+	     printM.setMaximumSize(BSize);
+	     printM.setAlignmentX(Component.CENTER_ALIGNMENT);
+	     
+	     printA = new JButton("Print Aktivitets Liste");
+	     printA.addActionListener(this);
+	     printA.setMaximumSize(BSize);
+	     printA.setAlignmentX(Component.CENTER_ALIGNMENT);
+	     
+	     printP = new JButton("Print Projekt Liste");
+	     printP.addActionListener(this);
+	     printP.setMaximumSize(BSize);
+	     printP.setAlignmentX(Component.CENTER_ALIGNMENT);
+	     
+	     printT = new JTextField();
+	     printT.setSize(300, 300);
 	     
 	     JPanel p1 = new JPanel();
 	     p1.setLayout(new BoxLayout(p1, BoxLayout.PAGE_AXIS));
 	     p1.add(Box.createRigidArea(new Dimension(spaceDimension)));
-	     p1.add(printK);
+	     p1.add(printM);
+	     p1.add(Box.createRigidArea(new Dimension(spaceDimension)));
+	     p1.add(printA);
+	     p1.add(Box.createRigidArea(new Dimension(spaceDimension)));
+	     p1.add(printP);
+	     p1.add(Box.createRigidArea(new Dimension(spaceDimension)));
+	     p1.add(printT);
 	     
 	     getContentPane().add(p1, BorderLayout.CENTER);
 	}
 	
 	public void actionPerformed(ActionEvent e)
 	{
+		printList print = new printList();
 		
-		
+		if(e.getSource() == printM)
+		{
+			print.printMedarbejderList();
+			
+		}
+		if(e.getSource() == printA)
+		{
+			print.printAktivitetList();
+		}
+		if(e.getSource() == printP)
+		{
+			print.printProjektList();
+		}
 	}
 }
