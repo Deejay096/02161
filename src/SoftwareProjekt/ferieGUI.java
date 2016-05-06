bdb58709b2d8a1f0da034f738e5cc485790a2fcf
ultracake <jackievuong@live.dk>
2016-05-06 17:20:21 +0200

    ja

--- src/SoftwareProjekt/ferieGUI.java
+++ src/SoftwareProjekt/ferieGUI.java
@@ -1,5 +1,18 @@
 package SoftwareProjekt;
 
-public class ferieGUI {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
 
+//Lavet af Jackie Vuong: studie nr. s153121 
+
+public class ferieGUI extends JFrame implements ActionListener
+{
+	
+	
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		
+	}
 }
